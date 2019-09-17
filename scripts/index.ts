3ac1eefcc39c2f5d7603dce49df4ed8f0da183c6
Sebastian Arthur Chua <sebastian.a.chua@gmail.com>
2019-09-17 15:42:18 +1000

    finished initial hero image with black overlay

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world');
+$(document).ready(() => {
+    console.log('Hello world');
+    // updateSliderImage();
+});
+
+// function updateSliderImage() {
+//     let img_ctr = 0;
+
+//     setInterval(() => {
+//         img_ctr = (img_ctr % 4) + 1;
+//         let img_url = '../assets/sliderimg/slide' + img_ctr + '.jpg';
+
+//         $('#sliderimg').css('animated fadeOut')
+//         $("#sliderimg").attr("src", img_url);
+//         $('#sliderimg').css('animated fadeIn')
+//     }, 3000);
+// }
