29c91d0e7a0e53acfd971118b2dcabd95ea33a50
Sebastian Arthur Chua <sebastian.a.chua@gmail.com>
2019-09-24 17:04:31 +1000

    integrated requirejs

--- scripts/about.ts
+++ scripts/about.ts
@@ -1,3 +1,6 @@
+import Navbar = require('./Navbar');
+import Footer = require('./Footer');
+
 let team = {
     "Executive": [
         {
@@ -127,7 +130,7 @@ let team = {
 };
 
 $(document).ready(() => {
-    new Navbar().addToContainer($('body'));
+    new Navbar('../assets/logo.png', 1, false, false).addToContainer($('body'));
     
     new MeetTheTeam(team).addToContainer($('#team'));
 
@@ -189,106 +192,4 @@ interface Committee {
     firstName: string;
     lastName: string;
     role: string;
-}
-
-class Navbar {
-    private _navbar: JQuery;
-
-    constructor() {
-        this._navbar = $('<nav>').addClass('navbar navbar-expand-xl navbar-light');
-        this._navbar.append(
-            '<a class="navbar-brand" href="#"><img id="logo" alt="Brand" src="../assets/logo.png"></a>\
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">\
-              <span class="navbar-toggler-icon"></span>\
-            </button>\
-          \
-            <div class="collapse navbar-collapse" id="navbarResponsive">\
-              <ul class="navbar-nav ml-auto">\
-                <li class="nav-item active">\
-                  <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>\
-                </li>\
-      \
-                <li class="nav-item">\
-                  <a class="nav-link" href="#">ABOUT US</a>\
-                </li>\
-      \
-                <li class="nav-item">\
-                  <a class="nav-link" href="#">MEMBERS BENEFITS</a>\
-                </li>\
-      \
-                <li class="nav-item">\
-                  <a class="nav-link" href="#">CAREERS</a>\
-                </li>\
-      \
-                <li class="nav-item">\
-                  <a class="nav-link" href="#">NEWSLETTER</a>\
-                </li>\
-      \
-                <li class="nav-item">\
-                  <a class="nav-link" href="#">ICON</a>\
-                </li>\
-      \
-                <li class="nav-item">\
-                  <a class="nav-link" href="#">CONTACT US</a>\
-                </li>\
-              </ul>\
-            </div>'
-        )
-    }
-
-    addToContainer(container: JQuery) {
-        container.prepend(this._navbar);
-    }
-}
-
-class Footer {
-    private _footer: JQuery;
-
-    constructor() {
-        this._footer = $('<div>').attr('id', 'footer-row').addClass('row');
-
-        let branding = $('<div>').attr('id', 'footer-branding').addClass('col-lg-3');
-        branding.append(
-            '<img id="footer-logo" class="img-fluid" alt="Brand" src="../assets/logo_red.png">\
-            <h2>PPIA <span style="color: #CA242B;">UNSW</span></h2>\
-            <p>UNSW Sydney,<br>Kensington, NSW<br>2052</p>\
-            <p style="font-size: 9px;"><i class="fas fa-copyright"></i> 2019 PPIA UNSW. All Rights Reserved.</p>'
-        );
-
-        let nav = $('<div>').attr('id', 'footer-nav').addClass('col-lg-3');
-        nav.append(
-            '<ul>\
-                <li><a class="nav-link active" href="#">Home</a></li>\
-                <li><a class="nav-link" href="#">About Us</a></li>\
-                <li><a class="nav-link" href="#">Members Benefits</a></li>\
-                <li><a class="nav-link" href="#">Careers</a></li>\
-                <li><a class="nav-link" href="#">Newsletter</a></li>\
-                <li><a class="nav-link" href="#">ICON</a></li>\
-                <li><a class="nav-link" href="#">Contact Us</a></li>\
-            </ul>'
-        );
-
-        let contact = $('<div>').attr('id', 'footer-contact').addClass('col-lg-6')
-        contact.append(
-            '<div id="partner-logos">\
-                <img src="../assets/garuda.png" class="img-fluid" alt="KJRI Logo">\
-                <img src="../assets/unsw.png" class="img-fluid" alt="UNSW Logo">\
-                <img src="../assets/arc.jpg" class="img-fluid" alt="Arc Logo" style="height: 60px;">\
-                <img src="../assets/ppia-nsw-new.png" class="img-fluid" alt="PPIA NSW Logo">\
-                <img src="../assets/ppia-logo.png" class="img-fluid" alt="PPIA Logo">\
-            </div>\
-            <div id="quick-links">\
-                <a href="mailto:unsw.ppia@gmail.com" class="fas fa-envelope"></a>\
-                <a href="https://www.facebook.com/groups/ppia.unsw/" class="fab fa-facebook-f"></a>\
-                <a href="https://www.instagram.com/ppiaunsw/" class="fab fa-instagram"></a>\
-                <a href="https://www.youtube.com/user/ppiaunsw" class="fab fa-youtube"></a>\
-            </div>'
-        );
-
-        this._footer.append(branding, nav, contact);
-    }
-    
-    addToContainer(container: JQuery) {
-        container.append(this._footer);
-    }
 }
